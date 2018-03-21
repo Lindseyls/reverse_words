f0d66704ff8ca2db2bbfd8208cb5232b46211091
Luxi Lindsey <Luxi.Lindsey@gmail.com>
2018-03-20 17:05:32 -0700

    Update reverse_words.rb

--- lib/reverse_words.rb
+++ lib/reverse_words.rb
@@ -26,37 +26,3 @@ def reverse_words(my_words)
   end
   return my_words
 end
-
-
-
-
-
-
-# def reverse_words(my_words)
-#   return if my_words == nil
-#   index = 0
-#   pointer = 0
-#   j = my_words.length
-#   while index < j
-#     if my_words[index] == " "
-#       my_words = rev_helper(pointer, index-1, my_words)
-#       pointer = -1
-#     end
-#     pointer += 1
-#     index += 1
-#   end
-#   my_words = rev_helper(pointer, index-1, my_words) if pointer > 0
-#   # binding.pry
-#   return my_words
-# end
-
-# def rev_helper(pointer, index, my_words)
-#   start_point = index
-#   while start_point < pointer / 2
-#     temp = my_words[start_point]
-#     my_words[start_point] = my_words[(pointer - 1) - start_point]
-#     my_words[(pointer - 1) - start_point] = temp
-#     start_point += i
-#   end
-#   return my_words
-# end
